77572b1fe6c0f8478b5c9b1fe5070b5d91b06f70
mohamedayari1 <ayarim781@gmail.com>
2025-07-17 21:38:53 +0100

    assets and layouts

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
+      <SignIn />
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
+      <SignUp />
+    </div>
+  );
 }
