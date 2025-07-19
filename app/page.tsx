5ff749f27ac8706f11300d221342fdbecd4b0cad
mohamedayari1 <ayarim781@gmail.com>
2025-07-19 14:59:53 +0100

    fix tailwind issue

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function Page() {
+  return (
+    <div>
+        <h1 className="h1-bold">
+            This is a piece of shit
+        </h1>
+        <h2 className="h2-bold">
+            This is a piece of shit
+        </h2>
+        <h3 className="h3-bold">
+            This is a piece of shit
+        </h3>
+    </div>
+  )
+}
+ 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,46 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
-          "100": "#FFF1E6",
-          "500": "#FF7000",
+          500: "#FF7000",
+          100: "#FFF1E6",
         },
         dark: {
-          "100": "#000000",
-          "200": "#0F1117",
-          "300": "#151821",
-          "400": "#212734",
-          "500": "#101012",
+          100: "#000000",
+          200: "#0F1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#101012",
         },
         light: {
-          "400": "#858EAD",
-          "500": "#7B8EC8",
-          "700": "#DCE3F1",
-          "800": "#F4F6F8",
-          "850": "#FDFDFD",
-          "900": "#FFFFFF",
+          900: "#FFFFFF",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
         },
-        link: {
-          "100": "#1DA1F2",
-        },
+        "accent-blue": "#1DA1F2",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        spaceGrotesk: ["var(--font-spaceGrotesk)"],
       },
       boxShadow: {
         "light-100":
@@ -41,26 +50,31 @@ const config: Config = {
         "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
         "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
       },
+      backgroundImage: {
+        "auth-dark": "url('/assets/images/auth-dark.png')",
+        "auth-light": "url('/assets/images/auth-light.png')",
+      },
       screens: {
         xs: "420px",
       },
-      fontFamily: {
-        inter: ["var(--font-inter)"],
-        "space-grotesk": ["var(--font-space-grotesk)"],
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      borderRadius: {
-        "2": "8px",
-        "1.5": "6px",
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      backgroundImage: {
-        "auth-dark": 'url("/images/auth-dark.png")',
-        "auth-light": 'url("/images/auth-light.png")',
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+  ],
 };
-export default config;
